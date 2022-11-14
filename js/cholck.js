220e408b371bffbd8996dd45e9751018acdc1359
sherali005 <abdillaevserali05@gmail.com>
2022-11-14 15:56:25 +0600

    add projekt git

---
+++ js/cholck.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    window.setInterval(function () {
+        const date = new Date();
+
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds()
+
+        const clock = hours + ":" + minutes + ":" + seconds;
+        document.getElementById("clock").innerHTML = clock
+    })
+}
